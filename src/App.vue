751c790dd7ca7c35889960139508cfaa78742908
sashoyusuke <zyesi731@gmail.com>
2021-03-21 11:11:30 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+   <input type="text" v-model="text">
+   <button @click="getaddress">住所入力</button>
+   <p>Address:{{address}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default{
+  data(){
+    return{
+      text:"",
+      address:""
+    };
+  },
+  async getaddress(){
+    const res =await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.text}?apiKey=bPRFvyBgjHxi0hvGJuY7O676Y9cNPnEVs8ejjmj`);
+    console.log(res);
+
+  }
+};
+</script>
+
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+// 追加
+import About from "../views/About.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  // 修正
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
